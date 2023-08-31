f129f2780261c92487fa1e5a957c6d9b1e7d3021
MathBergamo <matheusbergamo@hotmail.com>
2023-08-30 23:05:42 -0300

    commit inicial

---
+++ raizeslivres/src/main/java/com/generation/raizeslivres/RaizeslivresApplication.java
@@ -0,0 +1,13 @@
+package com.generation.raizeslivres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaizeslivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaizeslivresApplication.class, args);
+	}
+
+}
---
+++ raizeslivres/src/test/java/com/generation/raizeslivres/RaizeslivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.raizeslivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizeslivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
