605210432dca24c5d4511ca5d5d36b52fbf1f16a
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-09-05 15:50:03 -0300

    correcao

--- raizeslivres/src/main/java/com/generation/raizeslivres/Repository/ProdutoRepository.java
+++ raizeslivres/src/main/java/com/generation/raizeslivres/Repository/ProdutoRepository.java
@@ -3,6 +3,7 @@ package com.generation.raizeslivres.Repository;
 import com.generation.raizeslivres.Models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
