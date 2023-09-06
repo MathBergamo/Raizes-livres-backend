facafc3170a171467ec22b02b30f2fa0b89a66cf
unknown <luishenriquemf23@hotmail.com>
2023-09-06 08:29:04 -0300

    Task 03

---
+++ raizeslivres/src/main/java/com/generation/raizeslivres/Controller/CategoriaController.java
@@ -0,0 +1,14 @@
+package com.generation.raizeslivres.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+	
+	
+
+}
