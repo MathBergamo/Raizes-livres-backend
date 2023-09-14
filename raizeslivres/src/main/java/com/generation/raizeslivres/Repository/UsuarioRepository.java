ea336f3cb3a7b329546839ef28ee984d1d408a97
unknown <luishenriquemf23@hotmail.com>
2023-09-14 16:51:12 -0300

    security

--- raizeslivres/src/main/java/com/generation/raizeslivres/Repository/UsuarioRepository.java
+++ raizeslivres/src/main/java/com/generation/raizeslivres/Repository/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.generation.raizeslivres.Repository;
 
 import com.generation.raizeslivres.Models.Usuario;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional<Usuario> findByEmail(String email);
 
 }
